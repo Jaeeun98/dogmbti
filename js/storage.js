c1abf08e8c18ba87a04194189f1d2958ec9ad624
Jaeeun98 <wodms4525@naver.com>
2021-08-18 16:27:55 +0900

    TItle : 콘솔 삭제, Change

--- js/storage.js
+++ js/storage.js
@@ -4,8 +4,6 @@ const visitNum = document.querySelector('.visitNum');
 
 window.onload = () =>{
     const num = Number(localStorage.getItem('num'));
-    console.log(visitNum);
-    console.log(num);
     visitNum.innerText = `현재 참여자 수 : ${num}`;
 }
 
